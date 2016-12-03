84faedf256b34d9855f5a8b5e5544011991ad640
Nate Shih <nshih.85@gmail.com>
2016-12-02 20:15:38 -0500

    fixed orm

--- config/orm.js
+++ config/orm.js
@@ -8,35 +8,34 @@ var orm = {
 		// craft the query string based on the userID pulled from URL req.params
 		var queryString = 'SELECT * FROM PLAYERS WHERE id = ' + userID;
 		// connection query
-		connection.query(querystring, function (err, data) {
-		if (err) throw (err);
+		connection.query(queryString, function (err, data) {
 		// store the user's progression/clue number into var userClue for further use
 		var userClue = data.progression;
 		// console.log to test
 		console.log(data);
-		// return data as a json
-	    res.json(data);
+		console.log(userClue);
 		})
-	};
+	},
+	
 
 	// Checks the distance between user and location, needs to grab the clue's lat and lng from the CLUE_TABLE1 database.
 	checkDist: function (userClue) {
 		// craft the query string based on the player's current progression which is pulled from the previous function.
 		var queryString = 'SELECT * FROM CLUE_TABLE1 WHERE clue_num = ' + userClue;
 		// connection query
-		connection.query(querystring, function (err, data) {
-		if (err) throw (err);
+		connection.query(queryString, function (err, data) {
 		// console.log for test
 		console.log(data);
 		// store the lat and lng into variables
-		var	clueLat = data.lat,	
+		var	clueLat = data.lat
 		var	clueLng = data.lng
-		}
+		console.log("clueLat = " + clueLat + " clueLng = " + clueLng)
+		})
 		// Grab user's location from HTML5
 		navigator.geolocation.getCurrentPosition(function(position) {
-        var userLat = position.coords.latitude,
+        var userLat = position.coords.latitude
         var userLng = position.coords.longitude
-        };
+        })
         // checks the distance using gps-distance npm package
         var userDistance = distance(clueLat, clueLng, userLat, userLng);
 	}
